bf49118d837ff42d2c8fc37aba0037860cce23dd
devx-ph <zerodashpor@gmail.com>
2023-05-28 19:11:27 +0800

    added timeshift scripts

---
+++ shell_scripts/snapd.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo timeshift --list
+read "n?How many snapshot you want to delete? "
+
+if [[ -z "$n" ]] ; then
+  echo "Error. Enter a how many snapshots to delete!"
+else
+  for (( i=0; i < n; i++ )) ; do
+    read "d?Enter a name of snapshot you want to delete: " 
+
+    if [[ -z "$d" ]] ; then
+      echo "FAILED. Enter a name of snapshot you want to delete!"
+      break
+    else 
+      sudo timeshift --delete --snapshot "$d"
+      sudo grub-mkconfig -o /boot/grub/grub.cfg
+      echo "DONE. Snapshot $d deleted!"
+    fi
+    sudo timeshift --list
+  done
+fi
